b4702f0bcd8edf71f5b8086459ca42b1d457a7ca
Angela Cui <99048369+ayc325@users.noreply.github.com>
2024-07-12 07:21:43 -0700

    changed the map embed to look prettier

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,13 +5,11 @@ const Map = () => {
   
     return (
       <div className = "mapEmbed">
-            <iframe id = "googleMap" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3025.7485061009097!2d-74.00020842373941!3d40.6795099630568!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDQwJzQ2LjIiTiA3M8KwNTknNTAuMiJX!5e0!3m2!1sen!2sus!4v1720717653589!5m2!1sen!2sus" 
-            height="450"
+            <iframe id = "googleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.497084079858!2d-73.99986108809131!3d40.67950917127916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a585c181957%3A0x50e694b8c1ef75b2!2s2nd%20Pl%20%26%20Court%20St%2C%20Brooklyn%2C%20NY%2011231!5e0!3m2!1sen!2sus!4v1720793973440!5m2!1sen!2sus"  
+            height="450" 
             allowfullscreen="" 
             loading="lazy" 
-            referrerpolicy="no-referrer-when-downgrade">
-
-            </iframe>
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     )
   }
